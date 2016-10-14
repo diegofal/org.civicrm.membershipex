6bf80ce4b2a6e70eb2bc5d15c7886b607debfcb2
Diego Falciola <diego.falciola@gmail.com>
2016-10-14 17:19:27 -0300

    - Improved code readability - Removed hardcoded values - Added beforeEach to retrieve webpage

--- tests/e2e/controllerTest.js
+++ tests/e2e/controllerTest.js
@@ -1,34 +1,26 @@
 describe("Test Memberships Angular Extension", function () {
+  var baseUrl = 'http://d7-master.dev/civicrm/';
+  var extensionUrl = baseUrl + 'a/#/membersExtension';
 
   browser.ignoreSynchronization = true;
 
-  describe("Memberships List Menu", function () {
+  beforeEach(function(){
+    browser.driver.get(extensionUrl);
+  });
+  
+  it ("should exist the extension in the menu", function() {
+    var membershipExtensionElement = element(by.xpath('.//*[.="Memberships Angular List"]'));
+    expect(browser.isElementPresent(membershipExtensionElement)).toBe(true);
 
-    it ("should exists the extension in the menu", function() {
-
-      browser.driver.get('http://d7-master.dev/civicrm');
-
-      var membershipExtensionElement = element(by.xpath('.//*[.="Memberships Angular List"]'));
-
-      expect(browser.isElementPresent(membershipExtensionElement)).toBe(true);
-
-    });
   });
 
-
-  describe("Memberships Data table", function () {
-    
-    it ("should exists the table in the extension", function() {
-
-      browser.driver.get('http://d7-master.dev/civicrm/a/#/membersExtension');
-
-      membershipTablePresent = browser.wait(function(){
-        return element(by.id('membershipTable')).isPresent();
-      });
-
-      expect(membershipTablePresent).toBe(true);
-
+  it ("should exist the table in the extension", function() {
+    membershipTablePresent = browser.wait(function(){
+      return element(by.id('membershipTable')).isPresent();
     });
+
+    expect(membershipTablePresent).toBe(true);
+
   });
 
 });
